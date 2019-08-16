728269cf750d23aa971096814d51734c0e6b0b19
Tmax <Tmax@DESKTOP-9L3LF9D>
2019-08-16 18:01:52 +0900

    dd

--- src/main/java/com/example/demo/job/SimpleJobConfiguration2.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration2.java
@@ -1,7 +1,5 @@
 package com.example.demo.job;
 
-import java.util.Date;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -18,26 +16,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor //생성자 di
 @Configuration
-public class SimpleJobConfiguration {
+public class SimpleJobConfiguration2 {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
-	public Job simpleJob() {
-		return jobBuilderFactory.get("simpleJob")
-				.start(simpleStep1(null))
-				.next(simpleStep2(null))
+	public Job simpleJob2() {
+		return jobBuilderFactory.get("simpleJob2")
+				.start(simple2Step1())
+				.next(simple2Step2())
 				.build();
 	}
 	
 	@Bean
 	@JobScope
-	public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+	public Step simple2Step1() {
 		return stepBuilderFactory.get("simpleStep1")
 				.tasklet((contribution, chunkContext) -> {
-					log.info(">>>>> 스프링 배치 작업을 시작합니다. Step1");
-					log.info("requestDate="+requestDate);
+					log.info(">>>>> 스프링 배치 작업을 시작합니다. 2Step1");
 					return RepeatStatus.FINISHED;
 					//log.info("requestDate={}"+requestDate, requestDate);
 					//log.info("예외를 발생시킵니다.");
@@ -47,11 +44,10 @@ public class SimpleJobConfiguration {
 	
 	@Bean
 	@JobScope
-	public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+	public Step simple2Step2() {
 		return stepBuilderFactory.get("simpleStep2")
 				.tasklet((contribution, chunkContext) -> {
-					log.info(">>>>> 스프링 배치 작업을 시작합니다. Step2");
-					log.info("requestDate="+requestDate);
+					log.info(">>>>> 스프링 배치 작업을 시작합니다. 2Step2");
 					//log.info("requestDate={}"+requestDate, requestDate);
 					return RepeatStatus.FINISHED;
 				}).build();
